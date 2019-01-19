25f04667c5a49ff7d00024081f4787ac73ddefdd
Wilson Tran <2021tranw@kalanihs.org>
2019-01-18 20:18:31 -1000

    test2

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,10 +12,9 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.LDriveTrain_Subsystem;
-import frc.robot.subsystems.RDriveTrain_Subsystem;
+import frc.robot.subsystem.Hatch_Intake_Subsystem;
+
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -25,14 +24,12 @@ import frc.robot.subsystems.RDriveTrain_Subsystem;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
   public static OI m_oi;
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
-  public static RDriveTrain_Subsystem RDriveTrain = new RDriveTrain_Subsystem();
-  public static LDriveTrain_Subsystem LDriveTrain = new LDriveTrain_Subsystem();
+  
   public static Hatch_Intake_Subsystem Hatch_Intake = new Hatch_Intake_Subsystem(); 
 
   /**
