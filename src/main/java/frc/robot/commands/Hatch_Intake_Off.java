6eb5cc712698e46833eecfd2bef9ff515f4614f1
Wilson Tran <2021tranw@kalanihs.org>
2019-01-22 10:31:55 -1000

    didn't change anything

--- src/main/java/frc/robot/commands/Hatch_Intake_Off.java
+++ src/main/java/frc/robot/commands/Hatch_Intake_Off.java
@@ -31,7 +31,7 @@ public class Hatch_Intake_Off extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return false;  
   }
 
   // Called once after isFinished returns true
