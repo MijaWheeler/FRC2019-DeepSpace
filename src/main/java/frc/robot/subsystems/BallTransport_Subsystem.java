1cca2d6b9ebbbc526cd3aa26029683f5b71035b2
Kaikola <kaikodecker@gmail.com>
2019-01-23 20:32:53 -1000

    removed temp files

--- src/main/java/frc/robot/subsystems/BallTransport_Subsystem.java
+++ src/main/java/frc/robot/subsystems/BallTransport_Subsystem.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -9,10 +9,12 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+
 /**
- * An example subsystem.  You can replace me with your own Subsystem.
+ * Add your docs here.
  */
-public class HatchIntake_subsystem extends Subsystem {
+public class BallTransport_Subsystem extends Subsystem {
+  public WPI_TalonSRX Motor1 = new WPI_TalonSRX()
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
