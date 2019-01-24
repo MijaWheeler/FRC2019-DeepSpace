9c81e5da73ba1e4770afd5882759a41b24141084
Mija Wheeler <2021wheelerm@kalanihs.org>
2019-01-24 10:30:42 -1000

    Tunnel Code: no errors, ready to test/deploy [1/24 10:30am]

--- src/main/java/frc/robot/subsystems/Tunnel_Subsystem.java
+++ src/main/java/frc/robot/subsystems/Tunnel_Subsystem.java
@@ -6,26 +6,26 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
-import frc.robot.Robot;
+
+//import frc.robot.Robot;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Spark;
 
-//import edu.wpi.first.wpilibj.MotorSafety;;
-
-//import BAG motots
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
 public class Tunnel_Subsystem extends Subsystem {
-  public  TunBag = new BAGMotor(0, MotorType.kDC);
+
+  static Spark TunnelSpark = new Spark(9); 
   // Put methods for cont
 
   public void startTunnel(){ 
-		TunBag.set(0.75);
+		TunnelSpark.set(0.75);
 	}
 
 	public void stopTunnel(){ 
-		TunBag.set(0);
+		TunnelSpark.set(0);
 	}
 
 
