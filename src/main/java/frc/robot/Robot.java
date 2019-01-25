2718a4840edc2fef1640aceaee88eb04c87acfdf
Wilson Tran <2021tranw@kalanihs.org>
2019-01-24 18:54:01 -1000

    FINAL; WORKING

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,12 +13,12 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Encoder;
+// import frc.robot.subsystems.ExampleSubsystem;
+// import edu.wpi.first.wpilibj.Joystick;
+// import edu.wpi.first.wpilibj.command.Subsystem;
+// import edu.wpi.first.wpilibj.DigitalInput;
+// import edu.wpi.first.wpilibj.DriverStation;
+// import edu.wpi.first.wpilibj.Encoder;
 
 import frc.robot.subsystems.Hatch_Intake_Subsystem;
 
