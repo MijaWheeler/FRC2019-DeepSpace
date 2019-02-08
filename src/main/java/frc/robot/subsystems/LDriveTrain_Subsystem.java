1b2f579e143ebe32c67e04f16da1e6b26cbf3b4b
Anthony <2020laua@kalanihs.org>
2019-02-07 19:52:45 -1000

    added encoders

--- src/main/java/frc/robot/subsystems/LDriveTrain_Subsystem.java
+++ src/main/java/frc/robot/subsystems/LDriveTrain_Subsystem.java
@@ -13,6 +13,7 @@ import frc.robot.commands.LeftJoystick;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANEncoder;
 /**
  * Add your docs here.
  */
@@ -20,6 +21,7 @@ public class LDriveTrain_Subsystem extends Subsystem {
   public CANSparkMax MotorL1 = new CANSparkMax(RobotMap.MotorL1ID,MotorType.kBrushless);
   public CANSparkMax MotorL2 = new CANSparkMax(RobotMap.MotorL2ID,MotorType.kBrushless);
   public CANSparkMax MotorL3 = new CANSparkMax(RobotMap.MotorL3ID,MotorType.kBrushless);
+  public CANEncoder MotorL2_Encoder = MotorL2.getEncoder();
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
--- src/main/java/frc/robot/subsystems/RDriveTrain_Subsystem.java
+++ src/main/java/frc/robot/subsystems/RDriveTrain_Subsystem.java
@@ -13,14 +13,16 @@ import frc.robot.commands.RightJoystick;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANEncoder;
 
 /**
  * Add your docs here.
  */
 public class RDriveTrain_Subsystem extends Subsystem {
-  public CANSparkMax MotorR1 = new CANSparkMax(RobotMap.MotorR1ID, MotorType.kBrushless);
+  public CANSparkMax MotorR1 = new CANSparkMax(RobotMap.MotorR1ID,MotorType.kBrushless);
   public CANSparkMax MotorR2 = new CANSparkMax(RobotMap.MotorR2ID,MotorType.kBrushless);
   public CANSparkMax MotorR3 = new CANSparkMax(RobotMap.MotorR3ID,MotorType.kBrushless);
+  public CANEncoder MotorR2_Encoder = MotorR2.getEncoder();
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
