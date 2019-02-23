88f725fbe5ce664552c1ad90c7efbf57bda9a753
Wilson Tran <2021tranw@kalanihs.org>
2019-02-22 18:42:56 -1000

    should look around for changes in merging

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import frc.robot.commands.Box_Intake_On;
 import frc.robot.commands.Box_Outtake_On;
 import frc.robot.commands.Box_Off;
-
+import edu.wpi.first.wpilibj.GenericHID;
 
 
 /**
@@ -33,10 +33,9 @@ public class OI {
   // Button button = new JoystickButton(stick, buttonNumber)
   static Joystick stick0 = new Joystick(RobotMap.JoystickPort);
 
-  Button button1 = new JoystickButton(stick0, RobotMap.RMiddle);
-  // Button button2 = new JoystickButton(stick0, 0);
-  Button button3 = new JoystickButton(stick0, 3);
-
+  Button RMiddle = new JoystickButton(stick0, RobotMap.RMiddle);
+  JoystickAnalogButton RTrigger = new JoystickAnalogButton(stick0, RobotMap.RTrigger);
+  Button LMiddle = new JoystickButton(stick0, RobotMap.LMiddle);
   // There are a few additional built in buttons you can use. Additionally,
   // by subclassing Button you can create custom triggers and bind those to
   // commands the same as any other Button.
@@ -49,11 +48,11 @@ public class OI {
   // until it is finished as determined by it's isFinished method.
   // button.whenPressed(new ExampleCommand());
 
-public OI() {
-  button1.whileHeld(new Box_Intake_On());
-  // button2.whileHeld(new Box_Outtake_On());
-  button3.whenPressed(new Box_Off());
-}
+  public OI() {
+    RMiddle.whileHeld(new Box_Outtake_On());
+    RTrigger.whileHeld(new Box_Intake_On());
+    LMiddle.whileHeld(new Box_Off());
+  }
 
   // Run the command while the button is being held down and interrupt it once
   // the button is released.
@@ -62,4 +61,63 @@ public OI() {
   // Start the command when the button is released and let it run the command
   // until it is finished as determined by it's isFinished method.
   // button.whenReleased(new ExampleCommand());
+  public class JoystickAnalogButton extends Button {
+
+    GenericHID m_joystick;
+    int m_axisNumber;
+    private double THRESHOLD = 0.5;
+  
+    /**
+     * Create a button for triggering commands off a joystick's analog axis
+     * 
+     * @param joystick The GenericHID object that has the button (e.g. Joystick, KinectStick, etc)
+     * @param axisNumber The axis number
+     */
+    public JoystickAnalogButton(GenericHID joystick, int axisNumber) {
+        m_joystick = joystick;
+        m_axisNumber = axisNumber;
+    }
+  
+    /**
+     * Create a button for triggering commands off a joystick's analog axis
+     * 
+     * @param joystick The GenericHID object that has the button (e.g. Joystick, KinectStick, etc)
+     * @param axisNumber The axis number
+     * @param threshold The threshold to trigger above (positive) or below (negative)
+     */
+    public JoystickAnalogButton(GenericHID joystick, int axisNumber, double threshold) {
+      m_joystick = joystick;
+      m_axisNumber = axisNumber;
+      THRESHOLD = threshold;
+    }
+  
+    /**
+     * Set the value above which triggers should occur (for positive thresholds)
+     *  or below which triggers should occur (for negative thresholds)
+     * The default threshold value is 0.5
+     *  
+     * @param threshold the threshold value (1 to -1)
+     */
+    public void setThreshold(double threshold){
+      THRESHOLD = threshold;
+    }
+   
+    /**
+     * Get the defined threshold value.
+     * @return the threshold value
+     */
+    public double getThreshold(){
+      return THRESHOLD;
+    }
+    
+  
+    public boolean get() {
+      if(THRESHOLD < 0){
+        return m_joystick.getRawAxis(m_axisNumber) < THRESHOLD;    //Return true if axis value is less than negative threshold
+      } else {
+        return m_joystick.getRawAxis(m_axisNumber) > THRESHOLD;    //Return true if axis value is greater than positive threshold
+      }
+    }
+  
+  }
 }
